afad0ef32799ba98d37c30fabb16c0cd27c493c0
cfrman <jodyaliff@gmail.com>
2016-11-02 06:47:36 -0700

    Rename Windows Computers
    Rename windows computers using powershell

---
+++ Rename-loaner.ps1
@@ -0,0 +1,27 @@
+﻿ param(
+       [parameter(Mandatory=$True)]
+       $Computer,
+       [parameter(Mandatory=$True)]
+       $LNumber,
+       [parameter(Mandatory=$True)]
+       $OSType
+)
+
+#$AdComputer = Get-ADComputer -identity nuuuninu9128l3l
+#$code = "IT"
+
+$System = Get-WMIObject -ComputerName $Computer win32_computersystem 
+$Serial = Get-WMIObject -ComputerName $Computer win32_bios | Select -Expand Serialnumber 
+
+#$PCType = $System.PCSystemType
+#$PCType = Switch ($PCType) {
+#1 {"D"} #Desktop
+#2 {"L"} #Laptop 
+
+#}
+
+$GeneratedName = "NUUCIT-LOAN$LNumber$OSType"
+$GeneratedName
+
+Rename-Computer -NewName $GeneratedName -ComputerName $Computer -DomainCredential TFAYD\$env:UserName -restart -Confirm
+
---
+++ Rename-pc3.ps1
@@ -0,0 +1,25 @@
+﻿ param(
+       [parameter(Mandatory=$True)]
+       $Computer,
+       [parameter(Mandatory=$True)]
+       $OSType
+)
+
+#$AdComputer = Get-ADComputer -identity nuuuninu9128l3l
+#$code = "IT"
+
+$System = Get-WMIObject -ComputerName $Computer win32_computersystem 
+$Serial = Get-WMIObject -ComputerName $Computer win32_bios | Select -Expand Serialnumber 
+
+#$PCType = $System.PCSystemType
+#$PCType = Switch ($PCType) {
+#1 {"D"} #Desktop
+#2 {"L"} #Laptop 
+
+#}
+
+$GeneratedName = "NBCE-$Serial-$OSType"
+$GeneratedName
+
+Rename-Computer -NewName $GeneratedName -ComputerName $Computer -DomainCredential TFAYD\$env:UserName -restart -Confirm
+
